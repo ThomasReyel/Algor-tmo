81367996aff3a79e2ea09c7c97ff21acff57fd27
ThomasReyel <thomasreyel2231@gmail.com>
2025-04-15 14:43:43 -0300

    Update Calculador_de_raizes.jl

--- Calculador_de_raizes.jl
+++ Calculador_de_raizes.jl
@@ -1,24 +1,26 @@
-println("Por favor insira o valor de A da equação")
+println("Por favor insira o valor de A da equação ")
 coeficienteA = parse(Float64, readline())
 println("Por favor insira o valor de B da equação")
 coeficienteB = parse(Float64, readline())
 println("Por favor insira o valor de C da equação")
 coeficienteC = parse(Float64, readline())
 
-delta = (coeficienteB^2) - 4*coeficienteA * coeficienteB
-raiz1 = 0
-raiz2 = 0
+global delta = (coeficienteB^2) - 4*coeficienteA * coeficienteC
 
-if isinf(sqrt(delta))
-    println("O numero não tem raiz real")
-elseif delta - delta != 0
+if delta < 0
     println("O numero deu um delta negativo")
 elseif coeficienteA == 0
     println("O A da equação é zero")
 else
-    raiz1 = (-coeficienteB + sqrt(delta)) / 2*coeficienteA
-    raiz2 = (-coeficienteB - sqrt(delta)) / 2*coeficienteA
-    print(raiz1, raiz2)
+    try
+        global delta = parse(UInt64,delta)
+        raiz1 = (-coeficienteB + sqrt(delta)) / 2*coeficienteA
+        raiz2 = (-coeficienteB - sqrt(delta)) / 2*coeficienteA
+        print("As raízes são: ",raiz1," e " ,raiz2, ", ")
+    catch
+        println("Deu ruim, não tem raiz real")
+    finally
+        println("Até mais")
+    end
 end
 
-    
